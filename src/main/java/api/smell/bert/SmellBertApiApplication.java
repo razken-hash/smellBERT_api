8a93d9338ec7c0efc526d8a4d1f7e973c3dfd5ee
razken-hash <ka_kenniche@esi.dz>
2025-04-15 16:01:24 +0200

    SmellBERT SpringBoot API Initiated

---
+++ src/main/java/api/smell/bert/SmellBertApiApplication.java
@@ -0,0 +1,13 @@
+package api.smell.bert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmellBertApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmellBertApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/smell/bert/SmellBertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.smell.bert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmellBertApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
